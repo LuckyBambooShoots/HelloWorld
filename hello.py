6c7f031e94f0f83f63da1c573264c346dc840f08
LuckyBambooShoots <pohengleng@gmail.com>
2026-06-13 23:24:56 +0800

    Convert hello world to Flask app

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("🟡 Yellow World from Python")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🟡 Yellow World from AWS!"
+
+app.run(host="0.0.0.0", port=8080)
