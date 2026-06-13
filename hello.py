e014289fc06d46665e0a17100a56df92206f0e36
LuckyBambooShoots <pohengleng@gmail.com>
2026-06-13 23:35:40 +0800

    Update hello with time stamp

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🟡 Yellow World from AWS!"
+    return f"""
+    <h1>🟡 Yellow World from AWS!</h1>
+    <p>Current server time:</p>
+    <h2>{datetime.now()}</h2>
+    """
 
 app.run(host="0.0.0.0", port=8080)
